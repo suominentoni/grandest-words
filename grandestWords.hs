15704cd30c63016b0a59d279a95938027546bb44
Toni Suominen <toni.suominen@reaktor.com>
2015-05-25 23:47:02 +0300

    Fix bug: add handling for new second grandest, discarding previous second grandest

--- grandestWords.hs
+++ grandestWords.hs
@@ -13,11 +13,13 @@ getGrandestWords' :: [String] -> [String] -> [String] -> ([String], [String])
 getGrandestWords' mostGrand secondGrand [] = (mostGrand, secondGrand)
 getGrandestWords' mostGrand secondGrand [x]
   | getGrandnessValue x > getGrandnessValue (head mostGrand) = getGrandestWords' [x] mostGrand []
+  | getGrandnessValue x > getGrandnessValue (head secondGrand) = getGrandestWords' mostGrand [x] [] 
   | getGrandnessValue x == getGrandnessValue (head mostGrand) = getGrandestWords' (x:mostGrand) secondGrand [] 
   | getGrandnessValue x == getGrandnessValue (head secondGrand) = getGrandestWords' mostGrand (x:secondGrand) [] 
   | otherwise = getGrandestWords' mostGrand secondGrand []
 getGrandestWords' mostGrand secondGrand (x:xs)
   | getGrandnessValue x > getGrandnessValue (head mostGrand) = getGrandestWords' [x] mostGrand xs
+  | getGrandnessValue x > getGrandnessValue (head secondGrand) = getGrandestWords' mostGrand [x] [] 
   | getGrandnessValue x == getGrandnessValue (head mostGrand) = getGrandestWords' (x:mostGrand) secondGrand xs 
   | getGrandnessValue x == getGrandnessValue (head secondGrand) = getGrandestWords' mostGrand (x:secondGrand) xs 
   | otherwise = getGrandestWords' mostGrand secondGrand xs

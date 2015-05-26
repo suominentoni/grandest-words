fdee471667863be25cc85bcbd2edc4828a69022a
Toni Suominen <toni.suominen@reaktor.com>
2015-05-26 22:12:50 +0300

    Refactor test syntax to more compact form

--- test/grandestWordsTest.hs
+++ test/grandestWordsTest.hs
@@ -2,22 +2,15 @@ import Test.HUnit
 import qualified GrandestWords
 
 main = do
-  let test1 = TestCase $ assertEqual
-                         "Should return grandest words"
-                         (["grandest"], ["word."])
-                         (GrandestWords.getGrandestWords "A grandest word.")
-
-  let test2 = TestCase $ assertEqual
-                         "Should ignore case"
-                         (["Kick", "you,", "will"], ["I"])
-                         (GrandestWords.getGrandestWords "Kick you, I will")
-
-  let test3 = TestCase $ assertEqual
-                       "Should ignore numbers and special characters"
-                       (["and", "C-3PO"], ["R2-D2"])
-                       (GrandestWords.getGrandestWords "R2-D2 and C-3PO")
-
-  let tests = TestList [ "test1" ~: test1
-                       , "test2" ~: test2
-                       , "test3" ~: test3 ]
-  runTestTT tests
+  runTestTT $ TestList [ "test1" ~: assertEqual
+                                    "Should return grandest words"
+                                    (["grandest"], ["word."])
+                                    (GrandestWords.getGrandestWords "A grandest word.")
+                        , "test2" ~: assertEqual
+                                     "Should ignore case and duplicate letters"
+                                     (["Kick", "you,", "will"], ["I"])
+                                     (GrandestWords.getGrandestWords "Kick you, I will")
+                        , "test3" ~: assertEqual
+                                     "Should ignore numbers and special characters"
+                                     (["and", "C-3PO"], ["R2-D2"])
+                                     (GrandestWords.getGrandestWords "R2-D2 and C-3PO") ]

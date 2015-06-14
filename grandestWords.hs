3b7a6cf59ee2cc0dc42556099408429962b9b1b3
Toni Suominen <toni.suominen@reaktor.com>
2015-06-14 20:49:33 +0300

    Reduce counting grandess multiple times for same word

--- grandestWords.hs
+++ grandestWords.hs
@@ -12,34 +12,35 @@ main = do
   print $ nub $ getGrandestPairs contents
 
 getGrandestPairs :: String -> [(String, String)]
-getGrandestPairs contents = getGrandestPairs' (sortBy compareGrandness (words contents)) [("", "")]
+getGrandestPairs contents = getGrandestPairs' (sortBy compareGrandness wordsWithGrandness) [("", "")]
+  where wordsWithGrandness = map (\word -> (word, (getGrandness word))) (words contents)
 
-getGrandestPairs' :: [String] -> [(String, String)] -> [(String, String)]
+getGrandestPairs' :: [(String, Int)] -> [(String, String)] -> [(String, String)]
 getGrandestPairs' [] grandestPairs = grandestPairs
 getGrandestPairs' (x:xs) grandestPairs
   | sumLessThanGrandestPair x x grandestPairs = grandestPairs
   | otherwise = getGrandestPairs' xs (getGrandestPairsForAWord x xs grandestPairs)
 
-getGrandestPairsForAWord :: String -> [String] -> [(String, String)] -> [(String, String)]
+getGrandestPairsForAWord :: (String, Int) -> [(String, Int)] -> [(String, String)] -> [(String, String)]
 getGrandestPairsForAWord word [] grandestPairs = grandestPairs
 getGrandestPairsForAWord word (x:xs) grandestPairs
   | sumLessThanGrandestPair word x grandestPairs = grandestPairs
   | lessThanGrandestPair    word x grandestPairs = getGrandestPairsForAWord word xs grandestPairs
-  | greaterThanGrandestPair word x grandestPairs = getGrandestPairsForAWord word xs [(word,x)]
-  | equalToGrandestPair     word x grandestPairs = getGrandestPairsForAWord word xs (grandestPairs ++ [(word,x)])
+  | greaterThanGrandestPair word x grandestPairs = getGrandestPairsForAWord word xs [((fst word),(fst x))]
+  | equalToGrandestPair     word x grandestPairs = getGrandestPairsForAWord word xs (grandestPairs ++ [((fst word),(fst x))])
   | otherwise = getGrandestPairsForAWord word xs grandestPairs
 
-sumLessThanGrandestPair :: String -> String -> [(String, String)] -> Bool
-sumLessThanGrandestPair word1 word2 grandestPairs = ((getGrandness word1) + (getGrandness word2)) < getPairGrandness grandestPairs
+sumLessThanGrandestPair :: (String, Int) -> (String, Int) -> [(String, String)] -> Bool
+sumLessThanGrandestPair word1 word2 grandestPairs = ((snd word1) + (snd word2)) < getPairGrandness grandestPairs
 
-lessThanGrandestPair :: String -> String -> [(String, String)] -> Bool
-lessThanGrandestPair word1 word2 grandestPairs = ((getGrandness $ word1 ++ word2)) < getPairGrandness grandestPairs
+lessThanGrandestPair :: (String, Int) -> (String, Int) -> [(String, String)] -> Bool
+lessThanGrandestPair word1 word2 grandestPairs = ((getGrandness $ (fst word1) ++ (fst word2))) < getPairGrandness grandestPairs
 
-greaterThanGrandestPair :: String -> String -> [(String, String)] -> Bool
-greaterThanGrandestPair word1 word2 grandestPairs = (getGrandness $ word1 ++ word2) > getPairGrandness grandestPairs
+greaterThanGrandestPair :: (String, Int) -> (String, Int) -> [(String, String)] -> Bool
+greaterThanGrandestPair word1 word2 grandestPairs = (getGrandness $ (fst word1) ++ (fst word2)) > getPairGrandness grandestPairs
 
-equalToGrandestPair :: String -> String -> [(String, String)] -> Bool
-equalToGrandestPair word1 word2 grandestPairs = (getGrandness $ word1 ++ word2) == getPairGrandness grandestPairs
+equalToGrandestPair :: (String, Int) -> (String, Int) -> [(String, String)] -> Bool
+equalToGrandestPair word1 word2 grandestPairs = (getGrandness $ (fst word1) ++ (fst word2)) == getPairGrandness grandestPairs
 
 getPairGrandness :: [(String, String)] -> Int
 getPairGrandness grandestPairs = (getGrandness $ (fst $ head grandestPairs) ++ (snd $ head grandestPairs))
@@ -48,9 +49,9 @@ getGrandness :: String -> Int
 getGrandness word =
   length $ nub $ map toLower $ filter isLetter word
 
-compareGrandness :: String -> String -> Ordering
+compareGrandness :: (String, Int) -> (String, Int) -> Ordering
 compareGrandness a b
-  | getGrandness a < getGrandness b = GT
-  | getGrandness a > getGrandness b = LT
+  | snd a < snd b = GT
+  | (snd a) > (snd b) = LT
   | otherwise = EQ
 
